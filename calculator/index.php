4674c5111fa1d37772fed9a690c189d5a8fb4e11
S A G A R <bindusagar979@gmail.com>
2023-09-03 21:50:47 -0500

    calculaotr

---
+++ calculator/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator</title>
+    <style>
+        #main_block{
+            border: 5px solid;
+            width: 479px;
+            height: 600px;
+            position: absolute;
+            left: 450px;
+            top : 100px;
+
+        }
+        #plus{
+            top: 0px;
+        }
+    </style>
+</head> 
+<body>
+    <div id="main_block">
+        <input type="text" size=14 style="font-size:63.4px;border-bottom:5px solid"/>
+        <input type="button" value="1" style="font-size:40px;padding:40px 45px;"/>
+        <input type="button" value="2" style="font-size:40px;padding:40px 45px;"/>
+        <input type="button" value="3" style="font-size:40px;padding:40px 45px;"/>
+        <input type="button" value="+"  style="font-size:20px;padding:40px 45px;"/>
+        <input type="button" value="4" style="font-size:40px;padding:40px 45px;"/>
+        <input type="button" value="5" style="font-size:40px;padding:40px 45px;"/>
+        <input type="button" value="6" style="font-size:40px;padding:40px 45px;"/>
+        <input type="button" value="-"  style="font-size:20px;padding:40px 50px;"/>
+        <input type="button" value="7" style="font-size:40px;padding:40px 45px;"/>
+        <input type="button" value="8" style="font-size:40px;padding:40px 45px;"/>
+        <input type="button" value="9" style="font-size:40px;padding:40px 45px;"/>
+        <input type="button" value="*" style="font-size:20px;padding:40px 49px;"/>
+
+
+        <input type="button" value="=" style="font-size:40px;padding:40px 45px;margin-left:180px"/>
+
+    <div>
+    
+</body>
+</html>
